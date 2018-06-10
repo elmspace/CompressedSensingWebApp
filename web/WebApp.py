00e0d9b03a7331ca5586414ab463a3c8f1afadba
Ash Dehghan <elmspace@gmail.com>
2018-06-10 03:36:48 -0400

    Fixing some port/ip issues.

--- web/WebApp.py
+++ web/WebApp.py
@@ -101,7 +101,7 @@ def UploadFile():
 
 # Starting up a flask micro-service. 
 if __name__ == "__main__":
-	app.run(host='localhost', port=5000, threaded=True, debug=True);
+	app.run(host='0.0.0.0', port=5000, threaded=True, debug=True);
 
 
 
